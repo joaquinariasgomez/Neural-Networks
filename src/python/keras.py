4ff34337a6de6d55c53e1c0a92e158fabadaf09c
José Joaquín Arias <joaquinariasgomezcal@gmail.com>
2020-02-08 21:55:19 +0100

    Added fitting of the model and optional print of history results

--- src/python/keras.py
+++ src/python/keras.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+import pandas as pd
+import matplotlib.pyplot as plt
 from tensorflow import keras
 
 bd = keras.datasets.fashion_mnist #Fashion MNIST dataset
@@ -21,4 +23,13 @@ model.add(keras.layers.Dense(10, activation="softmax")) #Softmax because the cla
 
 model.compile(loss="sparse_categorical_crossentropy",
                 optimizer="sgd",    #This means we will train the model using simple Stochastic Gradient Descent (backpropagation algorithm)
+                                    #To tune the learing rate (which defaults to 0.01), use: optimizer=keras.optimizer.SGD(lr=???)
                 metrics=["accuracy"])
+
+history = model.fit(X_train, y_train, epochs=30,
+                    validation_data=(X_valid, y_valid))
+#Print results
+pd.DataFrame(history.history).plot()
+plt.grid(True)
+plt.gca().set_ylim(0, 1)    #Vertical range to [0-1]
+plt.show()
