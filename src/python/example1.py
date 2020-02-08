6d84fd28d012f90e80e44929e959a76510c2cb38
José Joaquín Arias <joaquinariasgomezcal@gmail.com>
2020-02-08 19:24:41 +0100

    Reorganized files to separate languages

--- src/python/example1.py
+++ src/python/example1.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.datasets import load_iris
 from sklearn.linear_model import Perceptron
 
@@ -6,11 +7,16 @@ iris = load_iris()
 X = iris.data[:, (2,3)]
 y = (iris.target == 0).astype(np.int)
 
-#print(X)
 print(y)
+plt.scatter(iris.data[:, 2], iris.data[:, 3], c=y)
 
 per_clt = Perceptron()
 per_clt.fit(X, y)
 
-y_pred = per_clt.predict([[2, 0.5]])
-print(y_pred)
+prediction = [1, 0.25]
+
+y_pred = per_clt.predict([prediction])
+
+plt.scatter(prediction[0], prediction[1], c='b')
+print(y_pred.tolist()[0])
+plt.show()
